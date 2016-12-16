7eff8bc176f4828d62b9a8ddbaf883be4059e05d
Maustemies <saku.rautio@luukku.com>
2016-12-16 21:01:42 +0200

    Added preliminary functionality for sensoring and made data structures for alarm handling.

--- app/src/main/java/maustemies/halyttaja/MainActivity.java
+++ app/src/main/java/maustemies/halyttaja/MainActivity.java
@@ -18,6 +18,97 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private class AlarmManager {
+        public boolean alarmOn = false;
+
+        public void StartAlarm() {
+            // TODO: Start the alarm
+            alarmOn = true;
+        }
+
+        public void StopAlarm() {
+            // TODO: Stop the alarm
+            alarmOn = false;
+        }
+    }
+    private AlarmManager alarmManager;
+
+    private class CustomSensorManager extends Thread implements SensorEventListener {
+
+        private SensorManager sensorManager;
+        private Sensor sensor;
+
+        private boolean newData = false;
+
+        private final static int accelerationValuesArraySize = 10;
+        private float[][] accelerationValues = new float[accelerationValuesArraySize][3];
+        private int accelerationValuesIndex = 0;
+
+        public CustomSensorManager() {
+            sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+            sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+            sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_FASTEST);
+        }
+
+        public void DisableSensorManagerListener() {
+            sensorManager.unregisterListener(this);
+        }
+
+        public void EnableSensorManagerListener() {
+            sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_FASTEST);
+        }
+
+        @Override
+        public void run() {
+
+            boolean accidentDetected = false;
+
+            while(true) {
+                if(newData) {
+                    // TODO: Iterate through the accelerationValues to see if there is an accident
+                    if(accidentDetected) {
+                        OnAccidentDetected();
+                        accidentDetected = false;
+                    }
+                    newData = false;
+                }
+            }
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            /**
+             * Algorithm taken from https://developer.android.com/guide/topics/sensors/sensors_motion.html
+             */
+            // In this example, alpha is calculated as t / (t + dT),
+            // where t is the low-pass filter's time-constant and
+            // dT is the event delivery rate.
+
+            final float alpha = 0.8f;
+            float[] gravity = new float[3];
+
+            // Isolate the force of gravity with the low-pass filter.
+            gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
+            gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
+            gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
+
+            // Remove the gravity contribution with the high-pass filter.
+            for(int i = 0; i < 3; i++) {
+                accelerationValues[accelerationValuesIndex][i] = event.values[i] - gravity[i];
+            }
+
+            accelerationValuesIndex++;
+            if(accelerationValuesIndex == accelerationValuesArraySize) accelerationValuesIndex = 0;
+            newData = true;
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    }
+    private CustomSensorManager customSensorManager;
+
     private RelativeLayout relativeLayout;
     private Button buttonStartStop;
     private TextView textViewStatus;
@@ -29,7 +120,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         InitViews();
-        //alarmManager = new AlarmManager();
+        alarmManager = new AlarmManager();
+        customSensorManager = new CustomSensorManager();
     }
 
     private void InitViews() {
@@ -47,15 +139,20 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void OnAccidentDetected() {
+        if(!alarmManager.alarmOn) alarmManager.StartAlarm();
+        UiOnAccidentDetected();
+    }
+
     private void OnButtonStartStopClicked() {
-        /*
         if(alarmManager.alarmOn) {
+            customSensorManager.DisableSensorManagerListener();
             UiOnAccidentDetectionTurnedOff();
         }
         else {
+            customSensorManager.EnableSensorManagerListener();
             UiOnAccidentDetectionTurnedOn();
         }
-        */
     }
 
     private void UiOnAccidentDetected() {
@@ -87,5 +184,19 @@ public class MainActivity extends AppCompatActivity {
 
     private void UiUpdateAdvicePressStopToStop() {
         textViewAdvice.setText(R.string.textAdvicePressStopToStopDetection);
+    }
+
+    @Override
+    protected void onResume() {
+        if(customSensorManager == null) return;
+
+        customSensorManager.EnableSensorManagerListener();
+    }
+
+    @Override
+    protected void onPause() {
+        if(customSensorManager == null) return;
+
+        customSensorManager.DisableSensorManagerListener();
     }
 }
